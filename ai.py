562c8bfeef6a455872d66125437893ed0140f1f7
Holden Lippert <holdenlippert@gmail.com>
2019-10-01 14:05:56 -0500

    big refactor to Game class

--- ai.py
+++ ai.py
@@ -1,26 +1,26 @@
-def maxvalue(state, gameover, score, agentActions, gameActions, result, depth):
-    if gameover(state) or depth == 0:
-        return score(state), None
+def maxvalue(state, depth):
+    if not state.actions() or depth == 0:
+        return state.score(), None
     value, bestAction = float("-inf"), None
-    for action in agentActions(state):
-        nextState = result(state, action)
+    for action in state.actions():
+        nextState = state.result(action)
         nextValue = expectedvalue(nextState, depth - 1)
         if nextValue > value:
             value, bestAction = nextScore, action
     return value, bestAction
 
-def expectedvalue(state, gameover, score, agentActions, gameActions, result, depth):
+def expectedvalue(state, depth):
     """
     Returns the tuple (score, bestMove).
     """
-    if depth == 0:
-        return score(state), None
+    if not state.actions() or depth == 0:
+        return state.score(), None
     scores = []
-    for action in gameActions(state):
-        nextState = result(state, action)
+    for action in state.actions():
+        nextState = state.result(action)
         nextValue = maxvalue(nextState, depth - 1)
         scores.append(nextValue)
     return sum(scores) / float(len(scores)), None
 
-def expectimax(state, gameover, score, actions, result, depth):
+def expectimax(state, depth):
     return maxvalue(state, depth)[1]
--- game.py
+++ game.py
@@ -1,72 +1,92 @@
 import ai
 import copy
+import random
 
 class Engine:
     def __init__(self, depth=3):
         self.depth = depth
-        self.state = [[0 for i in range(4)] for j in range(4)]
+        self.state = Game()
+        self.player = 0
+
+    def nextAction(self):
+        if self.state.player == 0:
+            return ai.expectimax(self.state, self.depth)
+        else:
+            return random.choice(self.state.gameActions())
 
     def get_next(self):
-        self.state = result(self.state, nextAction(self.state, self.depth))
-        return self.state
+        action = self.nextAction()
+        print("action:", action)
+        self.state = self.state.result(action)
+        return self.state.board
 
-def gameover(state):
-    return len(playerActions(state)) == 0
+class Game:
+    def __init__(self, width=4, height=4):
+        self.width, self.height = width, height
+        self.board = [[0 for i in range(width)] for j in range(height)]
+        self.player = 0
 
-def score(state):
-    maxvalue = float("-inf")
-    for row in state:
-        for cell in state:
-            maxvalue = max(maxvalue, cell)
-    return maxvalue
+    def __eq__(self, other):
+        return self.board == other.board
 
-def playerActions(state):
-    actions = []
-    for direction in ["up", "down", "left", "right"]:
-        action = ("shift", direction)
-        newstate = result(state, action)
-        if newstate != state:
-            actions.append(action)
-    return actions
+    def __ne__(self, other):
+        return not self == other
 
-def gameActions(state):
-    actions = []
-    for x in state:
-        for y in state[x]:
-            if state[x][y] == 0:
-                actions.append(("place", (x, y), 2))
-                actions.append(("place", (x, y), 4))
-    return actions
+    def score(self):
+        """
+        Returns the integer value of a given state. The current implementation
+        returns highest number present.
+        """
+        return max(map(max, self.board))
 
-def result(state, action):
-    if action[0] is "place":
-        x, y = action[1]
-        newstate = copy.deepcopy(state)
-        newstate[x][y] = action[2]
-        return newstate
-    elif action[1] is "shift":
-        return shift(state, direction)
-    else:
-        raise Exception("Received illegal action: " + action)
+    def actions(self):
+        return self.playerActions() if self.player == 0 else self.gameActions()
 
-def nextAction(state, depth):
-    return ai.expectimax(state, gameover, score, playerActions, gameActions, result, depth)
+    def playerActions(self):
+        return [("shift", direction)
+                for direction in ["up", "left", "down", "right"]
+                if self.result(("shift", direction)) != self]
 
-def shiftrow(row):
-    return row
+    def gameActions(self):
+        return [("place", (x, y), n)
+                for x in range(self.width)
+                for y in range(self.height)
+                for n in [2, 4]
+                if self.board[x][y] == 0]
 
-def shift(state, direction):
-    if direction is "up":
-        transpose = zip(*state)
-        newtranspose = shift(transpose, "left")
-        return zip(*transpose)
-    if direction is "down":
-        transpose = zip(*state)
-        newtranspose = shift(transpose, "right")
-        return zip(*transpose)
-    if direction is "left":
-        reverse = list(map(list, map(reversed, state)))
-        newreverse = shift(reverse, "right")
-        return list(map(list, map(reversed, state)))
-    if direction is "right":
-        return list(map(shiftrow, state))
+    def result(self, action):
+        self.player = (self.player + 1) % 2
+        return self
+
+
+# def result(state, action):
+    # if action is None:
+        # return state
+    # elif action[0] is "place":
+        # x, y = action[1]
+        # newstate = copy.deepcopy(state)
+        # newstate[x][y] = action[2]
+        # return newstate
+    # elif action[0] is "shift":
+        # return shift(state, action[1])
+    # else:
+        # raise Exception("Received illegal action: " + str(action))
+
+# def shiftrow(row):
+    # return row
+
+# def shift(state, direction):
+    # if direction is "up":
+        # transpose = list(zip(*state))
+        # newtranspose = shift(transpose, "left")
+        # return list(zip(*transpose))
+    # if direction is "down":
+        # transpose = list(zip(*state))
+        # newtranspose = shift(transpose, "right")
+        # return list(zip(*transpose))
+    # if direction is "left":
+        # reverse = list(map(list, map(reversed, state)))
+        # newreverse = shift(reverse, "right")
+        # return list(map(list, map(reversed, state)))
+    # if direction is "right":
+        # return list(map(shiftrow, state))
